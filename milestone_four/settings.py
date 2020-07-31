388c164692f32e40e508ebc7f784e69d2760180e
ADRIAN-PC\adria <adrian.havenga@yahoo.com>
2020-07-31 09:32:47 +0100

    added delete sslmode in DATABASES

--- milestone_four/settings.py
+++ milestone_four/settings.py
@@ -126,6 +126,7 @@ if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
+    del DATABASES['default']['OPTIONS']['sslmode']
 else:
     DATABASES = {
         'default': {
@@ -190,7 +191,7 @@ if 'USE_AWS' in os.environ:
 
     # Bucket configuration
     AWS_STORAGE_BUCKET_NAME = 'music-n-more'
-    AWS_S3_REGION_NAME = 'eu-west-2'
+    AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
