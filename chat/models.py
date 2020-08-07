ae20899c1abcd666682adab9907bb9f3bde34a89
ADRIAN-PC\adria <adrian.havenga@yahoo.com>
2020-08-07 15:11:53 +0100

    changed default chat image's type

--- chat/models.py
+++ chat/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Room(models.Model):
     title = models.CharField(max_length=255)
-    image = models.ImageField(default='chat_default.png',
+    image = models.ImageField(default='chat_default.jpg',
                               upload_to='chat_images')
 
     def __str__(self):
